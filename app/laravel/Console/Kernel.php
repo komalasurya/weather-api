c4ba34c1e0137257b60129bd187141f3209a5cc8
Iqbal Maulana <iq.bluejack@gmail.com>
2018-03-28 13:42:24 +0700

    Hot fix unregistered SetRequestForConsole bootstrapper

--- app/laravel/Console/Kernel.php
+++ app/laravel/Console/Kernel.php
@@ -20,8 +20,10 @@ class Kernel extends ConsoleKernel
         \App\Bootstrap\LoadConfiguration::class,
         \Illuminate\Foundation\Bootstrap\HandleExceptions::class,
         \Illuminate\Foundation\Bootstrap\RegisterFacades::class,
+        \Illuminate\Foundation\Bootstrap\SetRequestForConsole::class,
         \Illuminate\Foundation\Bootstrap\RegisterProviders::class,
         \Illuminate\Foundation\Bootstrap\BootProviders::class,
+
     ];
 
     /**
